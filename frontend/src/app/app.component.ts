02e2b2a3dbd27f90dd9c63159e9362dfa5e5319c
Vawi <val.winnen@gmail.com>
2018-11-01 13:50:43 +0100

    rxjs ok, tout fonctionne, affichage des titres des exams sur l'appli angular ok

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
-import {Subscription} from 'rxjs';
+import {Subscription} from 'rxjs/Subscription';
 import {ExamsApiService} from './exams/exams-api.service';
 import {Exam} from './exams/exam.model';
 
